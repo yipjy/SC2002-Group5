ba475b4d00ab5141cb8ec6a192602268ba781708
yipjy <yipjunyung@gmail.com>
2026-04-15 21:59:20 +0800

    Update Wizard.java

--- Wizard.java
+++ Wizard.java
@@ -1,4 +1,4 @@
-public class Wizard extends Player {
+public class Wizard extends Player {    // Author: JY
     public Wizard() {
         super("Wizard", 200, 50, 10, 20, new ArcaneBlast());
     }
