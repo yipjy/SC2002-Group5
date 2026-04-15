87a816bfa55c042aa4623470abbd513b0fdd75de
ramith001 <ramith001@e.ntu.edu.sg>
2026-04-15 12:08:32 +0800

    Add author comment to Action interface
    Added author comment to Action interface.

--- Action.java
+++ Action.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 
-public interface Action {
+public interface Action { // Author: R.V.
     void execute(Combatant user, List<Combatant> targets);
     boolean canUse(Combatant user);
     String getName();
